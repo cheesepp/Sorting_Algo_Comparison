8da649cc61f037a2ddbdda83d5712827c3e5e425
purifid-water <julianng1110@gmail.com>
2022-11-15 20:46:17 +0700

    first commit

---
+++ Sorting_Algo_Tuan.cpp
@@ -0,0 +1,38 @@
+/*Requirements
+    2 Mode comparison (so sánh 2 algo) và algorithm (chạy 1 algo)
+    1. Đọc input lấy dữ liệu -> sort -> chọn option (time hay comparison or both)
+    2. Chạy algo với size, order mình nhập
+    3. Chạy toàn bộ algo với size mình nhập
+    4. Compare 2 algo với file input
+    5. Compare 2 algo với size, order mình nhập
+*/
+/*Convention
+    Initialize functions
+    kiểu dữ liệu truyền vào các algo là int* &a, 
+        unsigned int &comparisonNum, với biến thời gian &timeCount
+    Gom phần helper của sort algo trong dấu //###############
+    Comment code
+*/
+/*Structure code
+    Ham main
+        Truyền tham chiếu cho biến comparisonNum, timeCount
+        Tao vector/mang chứa số comparison và time chạy cho từng thuật toán
+        Write file từng dòng là từng tên của algo và số comp và time
+*/
+
+/*Tuan's Part
+Selection
+Bubble
+Merge
+Radix
+Shell
+Flash
+*/
+
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+
+
+
